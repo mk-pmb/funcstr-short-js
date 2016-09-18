0318acfb0c3bb61abdc32dae170fe7f988c22aed
mk-pmb <mk-pmb@users.noreply.github.com>
2016-09-18 23:14:41 +0200

    maybe later

---
+++ funcstr-short.js
@@ -0,0 +1,26 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var EX;
+
+EX = function describeFunction(func) {
+  var descr = String(func).replace(/\s+/g, ' ').split(/\{/);
+
+  return descr.replace(/^function\s+/, EX.utf8ent.latinSmallFWithHook
+    ).substr(0, 32)
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = EX;
